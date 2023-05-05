e66d27e6649b3fde93ccf93b7952a069c39bd485
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-05-05 15:51:18 +0400

    Table names in lower case

--- sql/targetted/postgres.create.sql
+++ sql/targetted/postgres.create.sql
@@ -1,44 +1,44 @@
-CREATE TABLE t_range_pk_1M as SELECT
+CREATE TABLE t_range_pk_1m as SELECT
     s id,
     s v1,
     s v2
     FROM generate_series(1, 100000) s;
-ALTER TABLE t_range_pk_1M ADD PRIMARY KEY (id);
+ALTER TABLE t_range_pk_1m ADD PRIMARY KEY (id);
 
-CREATE VIEW t_hash_pk_1M AS SELECT * FROM t_range_pk_1M;
+CREATE VIEW t_hash_pk_1m AS SELECT * FROM t_range_pk_1m;
 
-CREATE TABLE t_range_pk_500K as SELECT
+CREATE TABLE t_range_pk_500k as SELECT
     s*2 id,
     s*2 v1,
     s*2 v2
     FROM generate_series(1, 50000) s;
-ALTER TABLE t_range_pk_500K ADD PRIMARY KEY (id);
+ALTER TABLE t_range_pk_500k ADD PRIMARY KEY (id);
 
-CREATE VIEW t_hash_pk_500K AS SELECT * FROM t_range_pk_500K;
+CREATE VIEW t_hash_pk_500k AS SELECT * FROM t_range_pk_500k;
 
-CREATE TABLE t_range_pk_200K as SELECT
+CREATE TABLE t_range_pk_200k as SELECT
     s*5 id,
     s*5 v1,
     s*5 v2
     FROM generate_series(1, 20000) s;
-ALTER TABLE t_range_pk_200K ADD PRIMARY KEY (id);
+ALTER TABLE t_range_pk_200k ADD PRIMARY KEY (id);
 
-CREATE VIEW t_hash_pk_200K AS SELECT * FROM t_range_pk_200K;
+CREATE VIEW t_hash_pk_200k AS SELECT * FROM t_range_pk_200k;
 
-CREATE TABLE t_range_pk_100K as SELECT
+CREATE TABLE t_range_pk_100k as SELECT
     s*10 id,
     s*10 v1,
     s*10 v2
     FROM generate_series(1, 10000) s;
-ALTER TABLE t_range_pk_100K ADD PRIMARY KEY (id);
+ALTER TABLE t_range_pk_100k ADD PRIMARY KEY (id);
 
-CREATE VIEW t_hash_pk_100K AS SELECT * FROM t_range_pk_100K;
+CREATE VIEW t_hash_pk_100k AS SELECT * FROM t_range_pk_100k;
 
-CREATE TABLE t_range_pk_1K as SELECT
+CREATE TABLE t_range_pk_1k as SELECT
     s*1000 id,
     s*1000 v1,
     s*1000 v2
     FROM generate_series(1, 100) s;
-ALTER TABLE t_range_pk_1K ADD PRIMARY KEY (id);
+ALTER TABLE t_range_pk_1k ADD PRIMARY KEY (id);
 
-CREATE VIEW t_hash_pk_1K AS SELECT * FROM t_range_pk_1K;
+CREATE VIEW t_hash_pk_1k AS SELECT * FROM t_range_pk_1k;
--- sql/targetted/queries/outer_table_index_scan_vs_seq_scan.sql
+++ sql/targetted/queries/outer_table_index_scan_vs_seq_scan.sql
@@ -1,50 +1,50 @@
-SELECT * FROM t_hash_pk_500K th500k JOIN t_hash_pk_1M th1M ON
+SELECT * FROM t_hash_pk_500k th500k JOIN t_hash_pk_1m th1M ON
     th500K.id = th1M.id;
 
-SELECT * FROM t_range_pk_500K tr500k JOIN t_hash_pk_1M th1M ON
+SELECT * FROM t_range_pk_500k tr500k JOIN t_hash_pk_1m th1M ON
     tr500K.id = th1M.id;
 
-SELECT * FROM t_hash_pk_500K th500k JOIN t_range_pk_1M tr1M ON
+SELECT * FROM t_hash_pk_500k th500k JOIN t_range_pk_1m tr1M ON
     th500K.id = tr1M.id;
 
-SELECT * FROM t_range_pk_500K tr500k JOIN t_range_pk_1M tr1M ON
+SELECT * FROM t_range_pk_500k tr500k JOIN t_range_pk_1m tr1M ON
     tr500k.id = tr1M.id;
 
 
-SELECT * FROM t_hash_pk_200K th200k JOIN t_hash_pk_1M th1M ON
+SELECT * FROM t_hash_pk_200k th200k JOIN t_hash_pk_1m th1M ON
     th200K.id = th1M.id;
 
-SELECT * FROM t_range_pk_200K tr200k JOIN t_hash_pk_1M th1M ON
+SELECT * FROM t_range_pk_200k tr200k JOIN t_hash_pk_1m th1M ON
     tr200K.id = th1M.id;
 
-SELECT * FROM t_hash_pk_200K th200k JOIN t_range_pk_1M tr1M ON
+SELECT * FROM t_hash_pk_200k th200k JOIN t_range_pk_1m tr1M ON
     th200K.id = tr1M.id;
 
-SELECT * FROM t_range_pk_200K tr200k JOIN t_range_pk_1M tr1M ON
+SELECT * FROM t_range_pk_200k tr200k JOIN t_range_pk_1m tr1M ON
     tr200k.id = tr1M.id;
 
 
-SELECT * FROM t_hash_pk_1K th100k JOIN t_hash_pk_1M th1M ON
+SELECT * FROM t_hash_pk_1k th100k JOIN t_hash_pk_1m th1M ON
     th100K.id = th1M.id;
 
-SELECT * FROM t_range_pk_100K tr100k JOIN t_hash_pk_1M th1M ON
+SELECT * FROM t_range_pk_100k tr100k JOIN t_hash_pk_1m th1M ON
     tr100K.id = th1M.id;
 
-SELECT * FROM t_hash_pk_100K th100k JOIN t_range_pk_1M tr1M ON
+SELECT * FROM t_hash_pk_100k th100k JOIN t_range_pk_1m tr1M ON
     th100K.id = tr1M.id;
 
-SELECT * FROM t_range_pk_100K tr100k JOIN t_range_pk_1M tr1M ON
+SELECT * FROM t_range_pk_100k tr100k JOIN t_range_pk_1m tr1M ON
     tr100k.id = tr1M.id;
 
 
-SELECT * FROM t_hash_pk_1K th1k JOIN t_hash_pk_1M th1M ON
+SELECT * FROM t_hash_pk_1k th1k JOIN t_hash_pk_1m th1M ON
     th1K.id = th1M.id;
 
-SELECT * FROM t_range_pk_1K tr1k JOIN t_hash_pk_1M th1M ON
+SELECT * FROM t_range_pk_1k tr1k JOIN t_hash_pk_1m th1M ON
     tr1K.id = th1M.id;
 
-SELECT * FROM t_hash_pk_1K th1k JOIN t_range_pk_1M tr1M ON
+SELECT * FROM t_hash_pk_1k th1k JOIN t_range_pk_1m tr1M ON
     th1K.id = tr1M.id;
 
-SELECT * FROM t_range_pk_1K tr1k JOIN t_range_pk_1M tr1M ON
+SELECT * FROM t_range_pk_1k tr1k JOIN t_range_pk_1m tr1M ON
     tr1k.id = tr1M.id;
