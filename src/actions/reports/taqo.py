0f35fc0d624d6dfff50b884140ca7a0284f8a7b5
Gaurav Kukreja <gaurav@gauravk.in>
2023-11-10 14:07:21 +0100

    report type supports Bokeh charts

--- src/actions/reports/taqo.py
+++ src/actions/reports/taqo.py
@@ -8,6 +8,14 @@ from objects import Query
 from actions.report import AbstractReportAction
 from utils import allowed_diff, get_plan_diff
 
+import numpy as np
+from bokeh.embed import components
+from bokeh.layouts import gridplot
+from bokeh.models import ColumnDataSource, HoverTool, TapTool, BoxZoomTool, WheelZoomTool, PanTool, SaveTool, ResetTool
+from bokeh.models import OpenURL, CDSView, GroupFilter
+from bokeh.plotting import figure
+from bokeh.transform import factor_cmap
+from scipy.stats import linregress
 
 class TaqoReport(AbstractReportAction):
     def __init__(self):
@@ -18,6 +26,8 @@ class TaqoReport(AbstractReportAction):
         self.failed_validation = []
         self.same_execution_plan = []
         self.better_plan_found = []
+        self.tagged_queries = {}
+        self.queries = []
 
     @classmethod
     def generate_report(cls, loq: CollectResult, pg_loq: CollectResult = None):
@@ -68,39 +78,146 @@ class TaqoReport(AbstractReportAction):
     def add_query(self, query: Type[Query], pg: Type[Query] | None):
         best_optimization = query.get_best_optimization(self.config)
 
-        if len(query.optimizations) > 1:
-            if self.config.compare_with_pg and query.result_hash != pg.result_hash:
-                self.failed_validation.append([query, pg])
-            if not self.config.compare_with_pg and query.result_hash != best_optimization.result_hash:
-                self.failed_validation.append([query, pg])
-
-            if allowed_diff(self.config, query.execution_time_ms,
-                            best_optimization.execution_time_ms):
-                self.same_execution_plan.append([query, pg])
-            else:
-                self.better_plan_found.append([query, pg])
+        if query.optimizations is None:
+            self.logger.info(f"No optimizations for '{query.query}'")
         else:
-            self.same_execution_plan.append([query, pg])
+            if len(query.optimizations) > 1:
+                self.queries.append(query)
+                if query.tag not in self.tagged_queries:
+                    self.tagged_queries[query.tag] = []
+                self.tagged_queries[query.tag].append([query, pg])
+                
+                # if self.config.compare_with_pg and query.result_hash != pg.result_hash:
+                #     self.failed_validation.append([query, pg])
+                # if not self.config.compare_with_pg and query.result_hash != best_optimization.result_hash:
+                #     self.failed_validation.append([query, pg])
+
+                # if allowed_diff(self.config, query.execution_time_ms,
+                #                 best_optimization.execution_time_ms):
+                #     self.same_execution_plan.append([query, pg])
+                # else:
+                #     self.better_plan_found.append([query, pg])
+            else:
+                self.same_execution_plan.append([query, pg])
+
+    def create_default_query_plots_interactive(self):
+        data = {
+            'query_hash': [],
+            'query_tag': [],
+            'query': [],
+            'yb_cost': [],
+            'yb_time': [],
+        }
+
+        data_yb_bad_plans = {
+            'yb_cost': [],
+            'yb_time': [],
+            'query_tag': [],
+        }
+
+        tags = []
+
+        for yb_query in self.queries:
+            if yb_query and yb_query.execution_time_ms:
+                data["query_hash"].append(yb_query.query_hash)
+                data["query_tag"].append(yb_query.tag)
+                tags.append(yb_query.tag)
+                data["query"].append(yb_query.query)
+                data["yb_cost"].append(yb_query.execution_plan.get_estimated_cost())
+                data["yb_time"].append(yb_query.execution_time_ms)
+                yb_best = yb_query.get_best_optimization(self.config)
+                if (not yb_query.compare_plans(yb_best.execution_plan)):
+                    data_yb_bad_plans["yb_cost"].append(yb_query.execution_plan.get_estimated_cost())
+                    data_yb_bad_plans["yb_time"].append(yb_query.execution_time_ms)
+                    data_yb_bad_plans["query_tag"].append(yb_query.tag)
+
+        source = ColumnDataSource(data)
+        source_yb_bad_plans = ColumnDataSource(data_yb_bad_plans)
+
+        TOOLTIPS = """
+            <div style="width:200px;">
+            @query
+            </div>
+        """
+        hover_tool = HoverTool(tooltips=TOOLTIPS)
+        hover_tool.renderers = []
+        TOOLS = [TapTool(), BoxZoomTool(), WheelZoomTool(), PanTool(), SaveTool(), ResetTool(), hover_tool]
+
+        tags = sorted(list(set(data['query_tag'])))
+
+        # YB Plot
+        yb_plot = figure(x_axis_label='Estimated Cost',
+                         y_axis_label='Execution Time (ms)',
+                         title='Yugabyte',
+                         width=1000, height=1000,
+                         tools=TOOLS, active_drag=None)
+
+        for tag in tags:
+            view = CDSView(filter=GroupFilter(column_name='query_tag', group=tag))
+            # Highliht queries with bad plans
+            yb_plot.scatter("yb_cost", "yb_time", size=14, line_width=4,
+                            source=source_yb_bad_plans, legend_label=tag, line_color='firebrick',
+                            color=None, fill_alpha=0.0, view=view)
+            # Scatter plot for all queries
+            yb_scatter = yb_plot.scatter("yb_cost", "yb_time", size=10, source=source,
+                                         hover_color="black", legend_label=tag, view=view,
+                                         color=factor_cmap('query_tag', 'Category20_20', tags),
+                                         selection_color='black', nonselection_alpha=1.0)
+            hover_tool.renderers.append(yb_scatter)
+
+        # Interactive Legend
+        yb_plot.legend.click_policy = 'hide'
+
+        # Linear Regression Line
+        yb_x_np = np.array(data['yb_cost'])
+        yb_y_np = np.array(data['yb_time'])
+        res = linregress(yb_x_np, yb_y_np)
+        yb_y_data_regress = res.slope * yb_x_np + res.intercept
+        yb_plot.line(x=yb_x_np, y=yb_y_data_regress)
+
+        # Tap event to jump to query
+        yb_url = 'tags/@query_tag.html#@query_hash'
+        yb_taptool = yb_plot.select(type=TapTool)
+        yb_taptool.callback = OpenURL(url=yb_url, same_tab=True)
+
+        script, div = components(yb_plot)
+        return script, div
 
     def build_report(self):
         # link to top
+        script, div = self.create_default_query_plots_interactive()
+        self.content += f"""
+++++
+<script type="text/javascript" src="https://cdn.bokeh.org/bokeh/release/bokeh-3.3.0.min.js"></script>
+<script type="text/javascript">
+    Bokeh.set_log_level("info");
+</script>
+{script}
+{div}
+++++
+"""
+        
         self.content += "\n[#top]\n== All results by analysis type\n"
         # different results links
         self.content += "\n<<result>>\n"
         self.content += "\n<<better>>\n"
         self.content += "\n<<found>>\n"
 
-        self.content += f"\n[#result]\n== Result validation failure ({len(self.failed_validation)})\n\n"
-        for query in self.failed_validation:
-            self.__report_query(query[0], query[1], True)
+        for tag in self.tagged_queries:
+            for query in self.tagged_queries[tag]:
+                self.__report_query(query[0], query[1], True)
 
-        self.content += f"\n[#better]\n== Better plan found queries ({len(self.better_plan_found)})\n\n"
-        for query in self.better_plan_found:
-            self.__report_query(query[0], query[1], True)
+        # self.content += f"\n[#result]\n== Result validation failure ({len(self.failed_validation)})\n\n"
+        # for query in self.failed_validation:
+        #     self.__report_query(query[0], query[1], True)
 
-        self.content += f"\n[#found]\n== No better plan found ({len(self.same_execution_plan)})\n\n"
-        for query in self.same_execution_plan:
-            self.__report_query(query[0], query[1], False)
+        # self.content += f"\n[#better]\n== Better plan found queries ({len(self.better_plan_found)})\n\n"
+        # for query in self.better_plan_found:
+        #     self.__report_query(query[0], query[1], True)
+
+        # self.content += f"\n[#found]\n== No better plan found ({len(self.same_execution_plan)})\n\n"
+        # for query in self.same_execution_plan:
+        #     self.__report_query(query[0], query[1], False)
 
     def __report_near_queries(self, query: Query):
         best_optimization = query.get_best_optimization(self.config)
